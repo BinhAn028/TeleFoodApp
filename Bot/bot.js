c1e32617af37c3e1dfda29392dac4f6da9ad2e0f
BinhAn028 <nguyenbinhan28021997@gmail.com>
2024-05-16 09:51:14 +0700

    UPdate Info

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,28 +1,49 @@
 const { Telegraf } = require("telegraf");
-const  { Keyboard } = require('telegram-keyboard')
-const TOKEN = "7011088128:AAGUjF7Wj7EOIdaHi--PXhq48rT9rJPZIos";
+const { Keyboard } = require('telegram-keyboard')
+const TOKEN = "7189895257:AAFjaEUl3mNtp1peoT5waBUrYNvfWerd8b8";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://tele-food-app.vercel.app/"
-bot.start((ctx) =>
-  ctx.reply("Welcome", {
-    reply_markup: {
-      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
-    },
-  })
+const web_link_2 = "https://public-terms-prove.loca.lt/"
+
+bot.start((ctx) => {
+    ctx.replyWithPhoto( "https://nemoverse.io/nemo_wallet.png",
+        {
+            caption:         'Your Wallet is set and ready. You can start using crypto in Telegram.',
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {
+                            text: 'Open Wallet',
+                            web_app: { url: web_link_2 }
+                        }]
+                ]
+            }
+        }
+    )
+}
 );
 
 bot.hears('text', async (ctx) => {
-  const keyboard = Keyboard.make([
-    ['Button 1', 'Button 2'], // First row
-    ['Button 3', 'Button 4'], // Second row
-  ])
-  
-  await ctx.reply('Simple built-in keyboard', keyboard.reply())
-  // await ctx.reply('choose' , keyboard.reply())
-  // await ctx.reply('Simple inline keyboard', keyboard.inline())
+    const user = ctx.from;
+    console.log(user);
+    ctx.reply(`Hello, ${user.first_name}!`);
+
+    const keyboard = Keyboard.make([
+        ['Button 1', 'Button 2'], // First row
+        ['Button 3', 'Button 4'], // Second row
+    ])
+    await ctx.reply('Simple built-in keyboard', keyboard.reply())
 })
 
-bot.hears('Search', ctx => ctx.reply('Yay!'))
+bot.on('message', (ctx) => {
+    ctx.reply("Welcome", {
+        reply_markup: {
+            keyboard: [
+                [{ text: "web app 2", web_app: { url: web_link_2 } }]
+            ],
+        },
+    })
+})
+
 
 bot.launch();
--- src/App.js
+++ src/App.js
@@ -9,67 +9,89 @@ const { getData } = require("./db/db");
 const foods = getData();
 
 const tele = window.Telegram.WebApp;
+const user = tele.initDataUnsafe?.user
+const {hash, auth_date} = tele.initDataUnsafe ?? {hash: '', auth_date: null}
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
 
-  useEffect(() => {
-    tele.ready();
-  });
+    useEffect(() => {
+        tele.ready();
+        console.log('annnnnnnnnnn user', user)
+        console.log('annnnnnnnnnn', tele.colorScheme)
+    });
 
-  const onAdd = (food) => {
-    const exist = cartItems.find((x) => x.id === food.id);
-    if (exist) {
-      setCartItems(
-        cartItems.map((x) =>
-          x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
-        )
-      );
-    } else {
-      setCartItems([...cartItems, { ...food, quantity: 1 }]);
-    }
-  };
+    const onAdd = (food) => {
+        const exist = cartItems.find((x) => x.id === food.id);
+        if (exist) {
+            setCartItems(
+                cartItems.map((x) =>
+                    x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
+                )
+            );
+        } else {
+            setCartItems([...cartItems, { ...food, quantity: 1 }]);
+        }
+    };
 
-  const onRemove = (food) => {
-    const exist = cartItems.find((x) => x.id === food.id);
-    if (exist.quantity === 1) {
-      setCartItems(cartItems.filter((x) => x.id !== food.id));
-    } else {
-      setCartItems(
-        cartItems.map((x) =>
-          x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
-        )
-      );
-    }
-  };
+    const onRemove = (food) => {
+        const exist = cartItems.find((x) => x.id === food.id);
+        if (exist.quantity === 1) {
+            setCartItems(cartItems.filter((x) => x.id !== food.id));
+        } else {
+            setCartItems(
+                cartItems.map((x) =>
+                    x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
+                )
+            );
+        }
+    };
 
-  const onCheckout = () => {
-    tele.MainButton.text = "Pay :)";
-    tele.MainButton.show();
-  };
-  // const linkToMarketPlace = () => {
-  //   tele.openMiniApp({
-  //     url: 'https://testnet.nemoverse.io',
-  //     data: {
-  //       user: 'test url'
-  //     }
-  //   })
-  // }
+    const onCheckout = () => {
+        tele.MainButton.text = "Pay :)";
+        tele.MainButton.show();
+    };
+    // const linkToMarketPlace = () => {
+    //   tele.openMiniApp({
+    //     url: 'https://testnet.nemoverse.io',
+    //     data: {
+    //       user: 'test url'
+    //     }
+    //   })
+    // }
 
-  return (
-    <>
-      <h1 className="heading">Order Food</h1>
-      <a href="https://testnet.nemoverse.io">Market Place</a>
-      <Cart cartItems={cartItems} onCheckout={onCheckout}/>
-      <div className="cards__container">
-        {foods.map((food) => {
-          return (
-            <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove} />
-          );
-        })}
-      </div>
-    </>
-  );
+    return (<>
+        <h1 className="heading" > Order Food </h1> <div onClick={
+            () => {
+                tele.sendData(JSON.stringify({ data: 'test2' }))
+            }
+        }>
+            <p>Info</p>
+            <p>ID: {user.id}</p>
+            <p>Last Name: {user.last_name}</p>
+            <p>FirstName: {user.first_name}</p>
+            <p>Username: {user.username}</p>
+            <p>Hash: {hash}</p>
+            <p>AuthDate: {auth_date}</p>
+            {/* {hash}
+            {authDate} */}
+        </div>
+        {/* <Cart
+            cartItems={cartItems}
+            onCheckout={onCheckout}
+        /> */}
+        <div className="cards__container" > {
+            foods.map((food) => {
+                return (<
+                    Card food={food}
+                    key={food.id}
+                    onAdd={onAdd}
+                    onRemove={onRemove}
+                />
+                );
+            })
+        } </div> </>
+    );
 }
 
 export default App;
